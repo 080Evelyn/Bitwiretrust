844d1c9c24016da4de037bb8ec9b818df3503462
Temidayo Gabriel <dayotoru@gmail.com>
2026-01-13 05:48:26 -0500

    update notification poppover and modal

--- src/Components/NotificationModal/NotificationModal.tsx
+++ src/Components/NotificationModal/NotificationModal.tsx
@@ -21,98 +21,97 @@ const NotificationModal = ({
 }: NotificationModalProps) => {
   return (
     <Dialog open={!!notification} onOpenChange={onClose}>
-      <DialogOverlay className="z-58">
-        <DialogContent className="z-60 w-full md:w-[398px] md:left-[48%] py-6.5 bg-[#FCF6FF]">
-          <DialogTitle className="sr-only">Notification</DialogTitle>
-          <DialogHeader className="flex items-center">
+      <DialogOverlay className="z-60" />
+      <DialogContent className="z-[60] w-full md:w-[398px] md:left-[48%] py-6.5 bg-[#FCF6FF]">
+        <DialogTitle className="sr-only">Notification</DialogTitle>
+        <DialogHeader className="flex items-center">
+          {notification?.status &&
+          notification?.status?.toLowerCase() === "success" ? (
+            <div className="bg-[#11C600]/50 rounded-full size-19 flex items-center justify-center">
+              <div className="bg-[#0FA301] size-15.5 rounded-full flex items-center justify-center">
+                <img src={naira} alt="naira" className="h-[25.54px]" />
+              </div>
+            </div>
+          ) : notification?.status &&
+            notification?.status?.toLowerCase() === "failed" ? (
+            <div className="bg-[#FF0000]/50 rounded-full size-19 flex items-center justify-center">
+              <div className="bg-[#FF0000] size-15.5 rounded-full flex items-center justify-center">
+                <X className="size-[42px] text-white" />
+              </div>
+            </div>
+          ) : (
+            <div className="bg-yellow-400/50 rounded-full size-19 flex items-center justify-center">
+              <div className="bg-yellow-400 size-15.5 rounded-full flex items-center justify-center">
+                <AlertTriangle className="size-[42px] text-white" />
+              </div>
+            </div>
+          )}
+          <div className="font-medium">
+            {notification?.title}{" "}
             {notification?.status &&
-            notification?.status?.toLowerCase() === "success" ? (
-              <div className="bg-[#11C600]/50 rounded-full size-19 flex items-center justify-center">
-                <div className="bg-[#0FA301] size-15.5 rounded-full flex items-center justify-center">
-                  <img src={naira} alt="naira" className="h-[25.54px]" />
-                </div>
-              </div>
-            ) : notification?.status &&
-              notification?.status?.toLowerCase() === "failed" ? (
-              <div className="bg-[#FF0000]/50 rounded-full size-19 flex items-center justify-center">
-                <div className="bg-[#FF0000] size-15.5 rounded-full flex items-center justify-center">
-                  <X className="size-[42px] text-white" />
-                </div>
-              </div>
-            ) : (
-              <div className="bg-yellow-400/50 rounded-full size-19 flex items-center justify-center">
-                <div className="bg-yellow-400 size-15.5 rounded-full flex items-center justify-center">
-                  <AlertTriangle className="size-[42px] text-white" />
-                </div>
-              </div>
-            )}
-            <div className="font-medium">
-              {notification?.title}{" "}
-              {notification?.status &&
-              notification?.status.toLowerCase() === "success"
-                ? "Successful"
-                : notification?.status &&
-                  notification?.status.toLowerCase() === "failed"
-                ? "Failed"
-                : "Pending"}
-            </div>
-            <h3 className="font-bold text-2xl">
-              {new Intl.NumberFormat("en-NG", {
-                style: "currency",
-                currency: "NGN",
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              }).format(notification?.amount || 0)}
-            </h3>
-          </DialogHeader>
-          <span className="border-2 border-[#7910B1] rounded-xs px-3 my-1.5" />
-          <div className="flex flex-col gap-2 text-sm font-medium">
-            <div className="flex justify-between">
-              <span>Transaction ID</span>
-              <span>{notification?.id}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Request ID</span>
-              <span>{notification?.requestId || "rxn-404"}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Date</span>
-              <span className="text-xs mt-2 text-right">
-                {notification && format(notification.createdAt, "MM-dd-yyyy")}
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Time</span>
-              <span>{notification && format(notification.createdAt, "p")}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Method</span>
-              <span>{notification?.transactionCategory}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Transaction Type</span>
-              <span>{notification?.type}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Status</span>
-              <span
-                className={cn(
-                  "capitalize",
-                  notification?.status &&
-                    notification.status.toLowerCase() === "success"
-                    ? "text-[#0FA301]"
-                    : notification?.status &&
-                      notification.status.toLowerCase() === "failed"
-                    ? "text-[#FF0000]"
-                    : "text-yellow-500"
-                )}
-              >
-                {notification?.status && notification.status.toLowerCase()}
-              </span>
-            </div>
+            notification?.status.toLowerCase() === "success"
+              ? "Successful"
+              : notification?.status &&
+                notification?.status.toLowerCase() === "failed"
+              ? "Failed"
+              : "Pending"}
           </div>
-        </DialogContent>
-      </DialogOverlay>
+          <h3 className="font-bold text-2xl">
+            {new Intl.NumberFormat("en-NG", {
+              style: "currency",
+              currency: "NGN",
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            }).format(notification?.amount || 0)}
+          </h3>
+        </DialogHeader>
+        <span className="border-2 border-[#7910B1] rounded-xs px-3 my-1.5" />
+        <div className="flex flex-col gap-2 text-sm font-medium">
+          {/* <div className="flex justify-between">
+            <span>Transaction ID</span>
+            <span>{notification?.id}</span>
+          </div> */}
+          <div className="flex justify-between">
+            <span>Request ID</span>
+            <span>{notification?.requestId || "rxn-404"}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Date</span>
+            <span className="text-xs mt-2 text-right">
+              {notification && format(notification.createdAt, "MM-dd-yyyy")}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span>Time</span>
+            <span>{notification && format(notification.createdAt, "p")}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Method</span>
+            <span>{notification?.transactionCategory}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Transaction Type</span>
+            <span>{notification?.type}</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Status</span>
+            <span
+              className={cn(
+                "capitalize",
+                notification?.status &&
+                  notification.status.toLowerCase() === "success"
+                  ? "text-[#0FA301]"
+                  : notification?.status &&
+                    notification.status.toLowerCase() === "failed"
+                  ? "text-[#FF0000]"
+                  : "text-yellow-500"
+              )}
+            >
+              {notification?.status && notification.status.toLowerCase()}
+            </span>
+          </div>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
